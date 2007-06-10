6241b470d23021cdba5c171817a0d939ed16b47c
Danny Baumann <dannybaumann@web.de>
2007-06-10 13:32:50 +0200

    Finally really fix w->output calculation.

--- src/group/group.c
+++ src/group/group.c
@@ -1379,16 +1379,15 @@ groupGetOutputExtentsForWindow (CompWindow * w, CompWindowExtents * output)
 		int glowTextureSize = gd->glowTextureProperties[glowType].textureSize;
 		int glowOffset = gd->glowTextureProperties[glowType].glowOffset;
 
-		glowSize = glowSize * glowOffset / glowTextureSize;
+		glowSize = glowSize * (glowTextureSize - glowOffset) / glowTextureSize;
 
-		if (glowSize > output->left)
-			output->left = glowSize;
-		if (glowSize > output->right)
-			output->right = glowSize;
-		if (glowSize > output->top)
-			output->top = glowSize;
-		if (glowSize > output->bottom)
-			output->bottom = glowSize;
+		/* glowSize is the size of the glow outside the window decoration
+		 * (w->input), while w->output includes the size of w->input
+		 * this is why we have to add w->input here */
+		output->left   = MAX (output->left, glowSize + w->input.left);
+		output->right  = MAX (output->right, glowSize + w->input.right);
+		output->top    = MAX (output->top, glowSize + w->input.top);
+		output->bottom = MAX (output->bottom, glowSize + w->input.bottom);
 	}
 }
 
