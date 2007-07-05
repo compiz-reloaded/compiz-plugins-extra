6159c8fb0c16179d0c11f86805510a484d10c437
Dennis Kasprzyk <onestone@opencompositing.org>
2007-07-05 15:56:11 +0200

    Compiz indent style.

--- src/crashhandler/crashhandler.c
+++ src/crashhandler/crashhandler.c
@@ -35,126 +35,141 @@ static CompDisplay *cDisplay;
 static void
 crash_handler (int sig)
 {
-	if (sig == SIGSEGV || sig == SIGFPE || sig == SIGILL || sig == SIGABRT)
+    if (sig == SIGSEGV || sig == SIGFPE || sig == SIGILL || sig == SIGABRT)
+    {
+	static int count = 0;
+
+	if (++count > 1)
+	    exit (1);
+
+	// backtrace
+	char cmd[1024];
+
+	sprintf (cmd, "echo -e \"set prompt\nthread apply all bt full\n"
+		      "echo \\\\\\n\necho \\\\\\n\nbt\nquit\" > /tmp/gdb.tmp;"
+		      "gdb -q %s %i < /tmp/gdb.tmp | "
+		      "grep -v \"No symbol table\" | "
+		      "tee /tmp/compiz_crash-%i.out; rm -f /tmp/gdb.tmp; "
+		      "echo \"\n[CRASH_HANDLER]: "
+		      "\\\"/tmp/compiz_crash-%i.out\\\" created!\n\"",
+		 programName, getpid (), getpid (), getpid () );
+
+	system (cmd);
+
+	if (crashhandlerGetStartWm (cDisplay) )
 	{
-		static int count = 0;
-
-		if (++count > 1)
-			exit (1);
-		// backtrace
-		char cmd[1024];
-
-		sprintf (cmd,
-				 "echo -e \"set prompt\nthread apply all bt full\necho \\\\\\n\necho \\\\\\n\nbt\nquit\" > /tmp/gdb.tmp; gdb -q %s %i < /tmp/gdb.tmp | grep -v \"No symbol table\" | tee /tmp/compiz_crash-%i.out; rm -f /tmp/gdb.tmp; echo \"\n[CRASH_HANDLER]: \\\"/tmp/compiz_crash-%i.out\\\" created!\n\"",
-				 programName, getpid (), getpid (), getpid ());
-		system (cmd);
-
-		if (crashhandlerGetStartWm (cDisplay))
-		{
-			if (fork () == 0)
-			{
-				setsid ();
-				putenv (cDisplay->displayString);
-				execl ("/bin/sh", "/bin/sh", "-c",
-					   crashhandlerGetWmCmd (cDisplay), NULL);
-				exit (0);
-			}
-		}
-
-		exit (1);
+	    if (fork () == 0)
+	    {
+		setsid ();
+		putenv (cDisplay->displayString);
+		execl ("/bin/sh", "/bin/sh", "-c",
+		       crashhandlerGetWmCmd (cDisplay), NULL);
+		exit (0);
+	    }
 	}
+
+	exit (1);
+    }
 }
 
 static void
-crashhandlerDisplayOptionChanged (CompDisplay * d, CompOption * opt,
-								  CrashhandlerDisplayOptions num)
+crashhandlerDisplayOptionChanged (CompDisplay                *d,
+				  CompOption                 *opt,
+				  CrashhandlerDisplayOptions num)
 {
-	switch (num)
+    switch (num)
+    {
+
+    case CrashhandlerDisplayOptionEnabled:
+	if (crashhandlerGetEnabled (d) )
 	{
-	case CrashhandlerDisplayOptionEnabled:
-		if (crashhandlerGetEnabled (d))
-		{
-			// enable crash handler
-			signal (SIGSEGV, crash_handler);
-			signal (SIGFPE, crash_handler);
-			signal (SIGILL, crash_handler);
-			signal (SIGABRT, crash_handler);
-		}
-		else
-		{
-			// disable crash handler
-			signal (SIGSEGV, SIG_DFL);
-			signal (SIGFPE, SIG_DFL);
-			signal (SIGILL, SIG_DFL);
-			signal (SIGABRT, SIG_DFL);
-		}
-		break;
-	default:
-		break;
+	    // enable crash handler
+	    signal (SIGSEGV, crash_handler);
+	    signal (SIGFPE, crash_handler);
+	    signal (SIGILL, crash_handler);
+	    signal (SIGABRT, crash_handler);
 	}
+	else
+	{
+	    // disable crash handler
+	    signal (SIGSEGV, SIG_DFL);
+	    signal (SIGFPE, SIG_DFL);
+	    signal (SIGILL, SIG_DFL);
+	    signal (SIGABRT, SIG_DFL);
+	}
+
+	break;
+
+    default:
+	break;
+    }
 }
 
 static Bool
-crashhandlerInitDisplay (CompPlugin * p, CompDisplay * d)
+crashhandlerInitDisplay (CompPlugin  *p,
+			 CompDisplay *d)
 {
-	cDisplay = d;
+    cDisplay = d;
 
-	if (crashhandlerGetEnabled (d))
-	{
-		// segmentation fault
-		signal (SIGSEGV, crash_handler);
-		// floating point exception
-		signal (SIGFPE, crash_handler);
-		// illegal instruction
-		signal (SIGILL, crash_handler);
-		// abort
-		signal (SIGABRT, crash_handler);
-	}
+    if (crashhandlerGetEnabled (d) )
+    {
+	// segmentation fault
+	signal (SIGSEGV, crash_handler);
+	// floating point exception
+	signal (SIGFPE, crash_handler);
+	// illegal instruction
+	signal (SIGILL, crash_handler);
+	// abort
+	signal (SIGABRT, crash_handler);
+    }
 
-	crashhandlerSetEnabledNotify (d, crashhandlerDisplayOptionChanged);
+    crashhandlerSetEnabledNotify (d, crashhandlerDisplayOptionChanged);
 
-	return TRUE;
+    return TRUE;
 }
 
 static void
-crashhandlerFiniDisplay (CompPlugin * p, CompDisplay * d)
+crashhandlerFiniDisplay (CompPlugin  *p,
+			 CompDisplay *d)
 {
-	signal (SIGSEGV, SIG_DFL);
-	signal (SIGFPE, SIG_DFL);
-	signal (SIGILL, SIG_DFL);
-	signal (SIGABRT, SIG_DFL);
+    signal (SIGSEGV, SIG_DFL);
+    signal (SIGFPE, SIG_DFL);
+    signal (SIGILL, SIG_DFL);
+    signal (SIGABRT, SIG_DFL);
 }
 
 static int
-crashhandlerGetVersion (CompPlugin * plugin, int version)
+crashhandlerGetVersion (CompPlugin *plugin,
+			int         version)
 {
-	return ABIVERSION;
+    return ABIVERSION;
 }
 
 CompPluginVTable crashhandlerVTable = {
-	"crashhandler",
-	crashhandlerGetVersion,
-	0,
-	0,
-	0,
-	crashhandlerInitDisplay,
-	crashhandlerFiniDisplay,
-	0,
-	0,
-	0,
-	0,
-	0,
-	0,
-	0,
-	0,
-	0,
-	0,
-	0,
-	0
+
+    "crashhandler",
+    crashhandlerGetVersion,
+    0,
+    0,
+    0,
+    crashhandlerInitDisplay,
+    crashhandlerFiniDisplay,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0
 };
 
 CompPluginVTable *
 getCompPluginInfo (void)
 {
-	return &crashhandlerVTable;
+    return &crashhandlerVTable;
 }
