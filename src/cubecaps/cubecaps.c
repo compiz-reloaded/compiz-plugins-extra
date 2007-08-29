fdfdc3fb200b4e0aa06f5992502e150e9cce150a
Guillaume Seguin <guillaume@segu.in>
2007-08-29 22:35:28 +0200

    * Use core utility functions for cube ABI checking

--- src/cubecaps/cubecaps.c
+++ src/cubecaps/cubecaps.c
@@ -654,26 +654,11 @@ cubecapsInitDisplay (CompPlugin  *p,
 		     CompDisplay *d)
 {
     CubeCapsDisplay *ccd;
-    CompPlugin	 *cube = findActivePlugin ("cube");
 
-    CompOption	*option;
-    int		nOption;
-
-    if (!cube || !cube->vTable->getDisplayOptions)
+    if (!checkPluginABI ("cube", CUBE_ABIVERSION))
 	return FALSE;
 
-    option = (*cube->vTable->getDisplayOptions) (cube, d, &nOption);
-
-    if (getIntOptionNamed (option, nOption, "abi", 0) != CUBE_ABIVERSION)
-    {
-	compLogMessage (d, "cubecaps", CompLogLevelError,
-			"cube ABI version mismatch");
-	return FALSE;
-    }
-
-    cubeDisplayPrivateIndex = getIntOptionNamed (option, nOption, "index", -1);
-
-    if (cubeDisplayPrivateIndex < 0)
+    if (!getPluginDisplayIndex (d, "cube", &cubeDisplayPrivateIndex))
 	return FALSE;
 
     ccd = malloc (sizeof (CubeCapsDisplay));
