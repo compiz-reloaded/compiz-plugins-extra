cd77904c93f4d8b7facee17bcd5beba8b90de460
Danny Baumann <dannybaumann@web.de>
2007-08-30 08:53:20 +0200

    Use core utility functions to retrieve ABI and display index.

--- src/cubereflex/cubereflex.c
+++ src/cubereflex/cubereflex.c
@@ -478,29 +478,14 @@ cubereflexInitDisplay (CompPlugin  *p,
 		       CompDisplay *d)
 {
     CubereflexDisplay *rd;
-    CompPlugin	      *cube = findActivePlugin ("cube");
 
-    CompOption	*option;
-    int		nOption;
-
-    if (!cube || !cube->vTable->getDisplayOptions)
+    if (!checkPluginABI ("cube", CUBE_ABIVERSION))
 	return FALSE;
 
-    option = (*cube->vTable->getDisplayOptions) (cube, d, &nOption);
-
-    if (getIntOptionNamed (option, nOption, "abi", 0) != CUBE_ABIVERSION)
-    {
-	compLogMessage (d, "cubereflex", CompLogLevelError,
-			"cube ABI version mismatch");
-	return FALSE;
-    }
-
-    cubeDisplayPrivateIndex = getIntOptionNamed (option, nOption, "index", -1);
-
-    if (cubeDisplayPrivateIndex < 0)
+    if (!getPluginDisplayIndex (d, "cube", &cubeDisplayPrivateIndex))
 	return FALSE;
 
-    rd = malloc (sizeof (CubereflexDisplay) );
+    rd = malloc (sizeof (CubereflexDisplay));
 
     if (!rd)
 	return FALSE;
