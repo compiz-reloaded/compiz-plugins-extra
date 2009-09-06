9ff4e50f1003160a4528869abc607053063b22e7
Erkin Bahceci <erkinbah@gmail.com>
2009-09-05 23:33:01 -0500

    Fix warning.

--- src/notification/notification.c
+++ src/notification/notification.c
@@ -319,8 +319,9 @@ notifyGetObjectOptions (CompPlugin *plugin,
 	(GetPluginObjectOptionsProc) notifyGetDisplayOptions
     };
 
+    *count = 0;
     RETURN_DISPATCH (object, dispTab, ARRAY_SIZE (dispTab),
-		     (void *) (*count = 0), (plugin, object, count));
+		     NULL, (plugin, object, count));
 }
 
 static CompBool
