a8647e48e01946187e8f4dc05cadfa7c914ab085
Scott Moreau <oreaus@gmail.com>
2014-12-03 12:28:41 -0700

    Add config.h to notification.c

--- src/notification/notification.c
+++ src/notification/notification.c
@@ -27,6 +27,8 @@
 #include <unistd.h>
 #include <compiz-core.h>
 
+#include "config.h"
+
 #define NOTIFY_DISPLAY_OPTION_TIMEOUT   0
 #define NOTIFY_DISPLAY_OPTION_MAX_LEVEL 1
 #define NOTIFY_DISPLAY_OPTION_NUM       2
