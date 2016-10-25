26ba16d0827fb1799df11e3c9ca0e877fc04d446
ethus3h <kolubat@gmail.com>
2016-10-24 22:38:23 -0400

    grid; scalefilter: more size options for high-DPI displays

--- src/grid/grid.c
+++ src/grid/grid.c
@@ -664,15 +664,16 @@ glPaintRectangle (CompScreen		  *s,
 	       ((float) gridGetOutlineColorBlue (s->display) / 65535.0f) * alpha,
 	       alpha);
 
-    glLineWidth (2.0);
+    int thickness = gridGetOutlineThickness (s->display);
+    glLineWidth (thickness);
     glBegin (GL_LINE_LOOP);
 
     /* set outline rect smaller to avoid damage issues */
     /* TODO: maybe get a better way of doing this */
-    glVertex2i (rect.x1 + 1, rect.y1 + 1);
-    glVertex2i (rect.x2 - 1, rect.y1 + 1);
-    glVertex2i (rect.x2 - 1, rect.y2 - 1);
-    glVertex2i (rect.x1 + 1, rect.y2 - 1);
+    glVertex2i (rect.x1 + (thickness * 0.5), rect.y1 + (thickness * 0.5));
+    glVertex2i (rect.x2 - (thickness * 0.5), rect.y1 + (thickness * 0.5));
+    glVertex2i (rect.x2 - (thickness * 0.5), rect.y2 - (thickness * 0.5));
+    glVertex2i (rect.x1 + (thickness * 0.5), rect.y2 - (thickness * 0.5));
     glEnd ();
 
     /* clean up */
