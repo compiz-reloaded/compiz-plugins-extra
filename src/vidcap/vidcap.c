66bb61d28b1c3a253cec8152dff15e901216ed2d
Scott Moreau <oreaus@gmail.com>
2017-09-10 17:20:42 -0600

    vidcap: Use O_LARGEFILE when opening output files
    Especially useful on 32bit where this flag permits files larger
    than 2GB.

--- src/vidcap/vidcap.c
+++ src/vidcap/vidcap.c
@@ -620,7 +620,7 @@ thread_func(void *data)
 	char filename[256], ext[32];
 	int i, j, ret, found;
 
-	fd = open(RAWFILE, O_WRONLY | O_CREAT | O_TRUNC | O_CLOEXEC, 0644);
+	fd = open(RAWFILE, O_WRONLY | O_CREAT | O_TRUNC | O_CLOEXEC | O_LARGEFILE, 0644);
 
 	write_file(fd);
 
@@ -755,7 +755,7 @@ vidcapToggle(CompDisplay     *d,
 		header.width = d->screens->width;
 		header.height = d->screens->height;
 
-		vd->fd = open(WCAPFILE, O_WRONLY | O_CREAT | O_TRUNC | O_CLOEXEC, 0644);
+		vd->fd = open(WCAPFILE, O_WRONLY | O_CREAT | O_TRUNC | O_CLOEXEC | O_LARGEFILE, 0644);
 
 		ret = write(vd->fd, &header, sizeof (header));
 
